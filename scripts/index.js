879ed02e49d846a9301415929c9e31c65f6aa8f2
Dmitriy S <andry_sila@mail.ru>
2025-01-21 02:44:45 +0500

    first rev

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
-// @todo: Темплейт карточки
-
-// @todo: DOM узлы
+const placesList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function cards(fullCard, deleteCallback) {
+  // @todo: Темплейт карточки
+  const template = document.querySelector("#card-template").content;
+  const cardElement = template.querySelector(".card").cloneNode(true);
+
+  // @todo: DOM узлы
+  const cardImage = cardElement.querySelector(".card__image");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  const likeButton = cardElement.querySelector(".card__like-button");
+  const cardTitle = cardElement.querySelector(".card__title");
+
+  cardImage.src = fullCard.link;
+  cardImage.alt = fullCard.name;
+  cardTitle.textContent = fullCard.name;
+
+  deleteButton.addEventListener("click", () => {
+    deleteCallback(cardElement);
+  });
+
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((fullCard) => {
+  const cardElement = cards(fullCard, deleteCard);
+  placesList.append(cardElement);
+});
